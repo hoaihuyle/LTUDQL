16f3e226bb52cef11262f7dbd67318fdc34b7035
hoaihuy <37462902+hoaihuy@users.noreply.github.com>
2019-02-27 00:19:16 +0700

    27/02/2019 12:19SA

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -19,6 +19,7 @@ namespace WindowsFormsApp1
         //Khai báo đối tượng kết nối
         SqlConnection con = new SqlConnection(@"Data Source=MY-1MMG59AB2LR7\SQLEXPRESS;Initial Catalog=QLBH;Integrated Security=True");
 
+        string sql;
 
         public Form1()
         {
@@ -30,7 +31,8 @@ namespace WindowsFormsApp1
             //Hiển thị dữ liệu ComboBox
 
             //Khởi tạo đối tượng dữ liệu
-            da = new SqlDataAdapter("Select * from NHA_CUNG_CAP", con);
+            sql = "Select * from KHACH_HANG";
+            da = new SqlDataAdapter(sql, con);
             //Khai báo đối tượng datatable để chứa dữ liệu
             DataTable dt = new DataTable();
             //Điền dữ liệu vào đối tượng Datatable
@@ -41,21 +43,17 @@ namespace WindowsFormsApp1
             //Gán dữ liệu nguồn
             comboBox1.DataSource = dt;
             //Gán trường sẽ hiển thị trên comboBox
-            comboBox1.DisplayMember = "Ten_NCC";
+            comboBox1.DisplayMember = "Ten_KH";
             //Gã trường mã ẩn sau mỗi trường trên comboBox
             //Nhằm mục đích khi chọn 1 item sẽ sử dụng mã ẩn để lọc dữ liệu 
-            comboBox1.ValueMember = "Ma_NCC";
+            comboBox1.ValueMember = "Ma_KH";
 
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            da = new SqlDataAdapter("Select m.Ten_MH,c.Ma_GH,c.Ma_MH,c.Sluong_giao,c.Ma_DH from MAT_HANG as m JOIN CHI_TIET_GH as c ON m.Ma_MH=c.Ma_MH WHERE m.Ma_NCC = N'" + comboBox1.SelectedValue + "'", con);
+            sql = "Select * FROM CHI_TIET_DH as c JOIN DON_HANG as d ON c.Ma_DH = d.Ma_DH  WHERE d.Ma_KH = N'" + comboBox1.SelectedValue + "'";
+            da = new SqlDataAdapter(sql, con);
             DataTable dt = new DataTable();
             da.Fill(dt);
             da.Dispose();
@@ -65,14 +63,14 @@ namespace WindowsFormsApp1
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //SqlConnection cn = new SqlConnection(con);
-            // Khởi tạo đối tượng đọc dữ liệu
-            da = new SqlDataAdapter("SELECT * FROM MAT_HANG WHERE Ma_NCC = N'" + comboBox1.SelectedValue+"'", con);
-            DataTable dt = new DataTable();
-            da.Fill(dt);
-            da.Dispose();
+            //sql= "SELECT * FROM MAT_HANG WHERE Ma_NCC = N'" + comboBox1.SelectedValue + "'"
+            //// Khởi tạo đối tượng đọc dữ liệu
+            //da = new SqlDataAdapter(sql, con);
+            //DataTable dt = new DataTable();
+            //da.Fill(dt);
+            //da.Dispose();
 
-            dataGridView1.DataSource = dt;
+            //dataGridView1.DataSource = dt;
         }
 
     }
