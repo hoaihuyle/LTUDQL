a6e694acbf1fd666f785e0294dc62e04b3609692
hoaihuyle <hoaihuy2011.vn@gmail.com>
2019-02-27 09:36:44 +0700

    27/02/2019

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -17,7 +17,7 @@ namespace WindowsFormsApp1
         //Khai báo đối tượng dùng để đọc dữ liệu bảng 
         SqlDataAdapter da;
         //Khai báo đối tượng kết nối
-        SqlConnection con = new SqlConnection(@"Data Source=MY-1MMG59AB2LR7\SQLEXPRESS;Initial Catalog=QLBH;Integrated Security=True");
+        SqlConnection con = new SqlConnection(@"Data Source=A5233\MISASME2017;Initial Catalog=QLBH;Integrated Security=True");
 
         string sql;
 
@@ -31,7 +31,7 @@ namespace WindowsFormsApp1
             //Hiển thị dữ liệu ComboBox
 
             //Khởi tạo đối tượng dữ liệu
-            sql = "Select * from KHACH_HANG";
+            sql = "Select * from NHA_CUNG_CAP";
             da = new SqlDataAdapter(sql, con);
             //Khai báo đối tượng datatable để chứa dữ liệu
             DataTable dt = new DataTable();
@@ -43,16 +43,16 @@ namespace WindowsFormsApp1
             //Gán dữ liệu nguồn
             comboBox1.DataSource = dt;
             //Gán trường sẽ hiển thị trên comboBox
-            comboBox1.DisplayMember = "Ten_KH";
+            comboBox1.DisplayMember = "Ten_NCC";
             //Gã trường mã ẩn sau mỗi trường trên comboBox
             //Nhằm mục đích khi chọn 1 item sẽ sử dụng mã ẩn để lọc dữ liệu 
-            comboBox1.ValueMember = "Ma_KH";
+            comboBox1.ValueMember = "Ma_NCC";
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            sql = "Select * FROM CHI_TIET_DH as c JOIN DON_HANG as d ON c.Ma_DH = d.Ma_DH  WHERE d.Ma_KH = N'" + comboBox1.SelectedValue + "'";
+            sql = "SELECT * FROM dbo.MAT_HANG LEFT JOIN dbo.CHI_TIET_DH ON dbo.MAT_HANG.Ma_MH = dbo.CHI_TIET_DH.Ma_MH  WHERE dbo.MAT_HANG.Ma_NCC = N'" + comboBox1.SelectedValue + "'";
             da = new SqlDataAdapter(sql, con);
             DataTable dt = new DataTable();
             da.Fill(dt);
