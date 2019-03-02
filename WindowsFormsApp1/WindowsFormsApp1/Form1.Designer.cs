771fa670d10328d15fdd09ce4de6b6529ebe58e8
hoaihuy <37462902+hoaihuy@users.noreply.github.com>
2019-03-02 08:26:04 +0700

    02/03/2019 8:25 SA

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -162,7 +162,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 25);
             this.button2.TabIndex = 7;
-            this.button2.Text = "Clear";
+            this.button2.Text = "Cập nhập";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
